ade02bfb1cfed37980f975b5c11178edd4e8c0b2
Touche123 <jonaswestman@hotmail.com>
2014-10-01 00:27:31 +0200

    Signed-off-by: Touche123

--- SRM-150-DIV2-250.h
+++ SRM-150-DIV2-250.h
@@ -9,18 +9,16 @@ public:
     int days(vector<int> arrivals, int numPerDay)
     {
         int totalRepairDays = 0;
-        int temp = 0;
         for (auto & it : arrivals)
         {
-            temp = it - numPerDay;
             if (it > 0){
                 totalRepairDays++;
             }
-            while (temp > 0){
+            it -= numPerDay;
+            while (it > 0){
                 totalRepairDays++;
-                temp -= numPerDay;
+                it -= numPerDay;
             }
-            temp = 0;
         }
         return totalRepairDays;
     }
